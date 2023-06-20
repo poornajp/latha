dbc153f4b7ccb8fb44f83c479130b6b0ba5f672e
poornajp <129258646+poornajp@users.noreply.github.com>
2023-06-20 21:15:46 +0530

    Update one.java

--- one.java
+++ one.java
@@ -3,6 +3,7 @@ public class hello java
 	public static void main (string[]args)
 	{
 		System.out.println("Hello java");
-		System.oou.println("Welcome to se lab");
+		System.out.println("Welcome to se lab");
+		System.out.println("Siddaganga polytechnic");
 	}
 }
